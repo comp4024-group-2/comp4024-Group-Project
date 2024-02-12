5fc852dea3af1e65b3e538d0ae0ab0228aa40ee6
psyjd8 <psyjd8@nottingham.ac.uk>
2024-02-12 18:48:47 +0000

    Added basic player movement where the character stands on a platform

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    public bool isJumping; // a bool means is it a true or false statement
+    private float playersMove;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // The rigid body 2d allows an object to have simulating physical interactions
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this gets the arrow keys or Awsd keys to move the character left or right
+        playersMove = Input.GetAxis("Horizontal");
+
+        // this moves the character left or right depending on the input speed
+        rb.velocity = new Vector2(speed * playersMove, rb.velocity.y);
+
+        // if the charcter is not jumping and the player is pressing the jump button
+        if (Input.GetButtonDown("Jump") && isJumping == false) {
+            // this allows the character to jump
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            Debug.Log("Jump"); // example in how to leave debug statements
+        }
+    }
+
+    // this function checks if the user is on the ground if so then the charcter isnt jumping
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    // this function checks if the user is off the ground if so then the charcter is jumping
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+}
