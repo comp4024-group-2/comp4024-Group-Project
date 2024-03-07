c825efa1be347092d73acdbc860f0189ef0fa76a
Ivan Garrahan <psyig2@exmail.nottingham.ac.uk>
2024-03-07 09:34:50 +0000

    fix: Fixed lose screen buttons

--- Assets/Scripts/WinLoseSceneButtons.cs
+++ Assets/Scripts/WinLoseSceneButtons.cs
@@ -8,17 +8,19 @@ public class WinLoseSceneButtons : MonoBehaviour
     // Start is called before the first frame update
     public void LoadMainScene()
     {
+        Debug.Log(gameObject + ": Main Menu Button clicked");
         SceneManager.LoadScene("Main");
     }
 
     public void LoadFirstLevel()
     {
+        Debug.Log(gameObject + ": Try again Button clicked");
         SceneManager.LoadScene("CollatedScene");
     }
 
     public void QuitGame()
     {
-        Debug.Log("Quitting Game");
+        Debug.Log(gameObject + ": Quit Button clicked");
         Application.Quit(); // Quits the application
         Debug.Log("Game is exiting");
     }
